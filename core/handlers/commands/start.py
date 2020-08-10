a538105e97779ce724b39fa096d75f6ebc5d06d1
hexatester <revolusi147id@gmail.com>
2020-08-10 15:52:45 +0700

    improve start command

--- core/handlers/commands/start.py
+++ core/handlers/commands/start.py
@@ -1,10 +1,15 @@
-from telegram import Update
+from telegram import Update, User
 from telegram.ext import CallbackContext
-
-WELCOME_MESSAGE = '''
-Selamat datang di bot @UniversitasTerbukaBot
-'''
 
 
 def start(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(WELCOME_MESSAGE)
+    user: User = update.effective_user
+    update.effective_message.reply_text(
+        f'Selamat datang {user.full_name}\n\n'
+        'Daftar Perintah\n'
+        '/start - Memulai bot\n'
+        '/shortlink - Memendekan url dengan https://sl.ut.ac.id/\n'
+        '/link - Daftar link UT\n'
+        '/formulir - Daftar Formulir\n'
+        '/dev - Daftar Pengembang\n'
+    )
