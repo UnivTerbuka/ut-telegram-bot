4aa506d8c7a7588031eb77c3d6bb991f2b2880e4
hexatester <revolusi147id@gmail.com>
2020-08-10 15:16:22 +0700

    add links

--- core/handlers/commands/link.py
+++ core/handlers/commands/link.py
@@ -1,11 +1,23 @@
 from telegram import Update, Bot
 from telegram.ext import CallbackContext
 
-LINKS = []
-
 MESSAGE = '''
-Berikut link-link web universitas terbuka
-Formulir | Universitas Terbuka
+<a href="https://www.ut.ac.id/">Universitas Terbuka</a>
+
+<a href="https://www.ut.ac.id/jaminan-kualitas/akreditasi-ban-pt">Akreditasi BAN-PT</a>
+<a href="https://www.ut.ac.id/biaya-pendidikan">Biaya Pendidikan</a>
+<a href="https://www.ut.ac.id/katalog">Katalog</a>
+<a href="https://sia.ut.ac.id/register">Registrasi Online</a>
+<a href="https://www.ut.ac.id/formulir">Formulir</a>
+
+<a href="http://elearning.ut.ac.id/">Tutorial Online (Elearning UT)</a>
+<a href="https://www.ut.ac.id/berita">Berita</a>
+<a href="https://www.ut.ac.id/online-learning">Online Learning</a>
+<a href="http://ecampus.ut.ac.id/">Ecampus</a>
+<a href="https://www.ut.ac.id/pengumuman">Pengumuman</a>
+
+<a href="http://hallo-ut.ut.ac.id/">Layanan Informasi dan Bantuan</a>
+<a href="https://www.ut.ac.id/contact-center">Contact Center</a>
 '''
 
 
