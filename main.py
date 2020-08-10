b92643fc180a94923ffbcf91e91d82c8c8dbd9a1
hexatester <revolusi147id@gmail.com>
2020-08-10 15:16:35 +0700

    fix local

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
-    NAME = os.environ.get('NAME')
     TOKEN = os.environ.get('TOKEN')
-    ut_bot = UniversitasTerbukaBot(TOKEN, NAME)
+    ut_bot = UniversitasTerbukaBot(TOKEN)
     ut_bot.polling()
