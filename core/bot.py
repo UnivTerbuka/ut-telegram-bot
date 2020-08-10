4ecdd414a91c9367a9494236109700e8f6faa3cb
hexatester <revolusi147id@gmail.com>
2020-08-10 15:25:48 +0700

    fix bot

--- core/bot.py
+++ core/bot.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from queue import Queue
 
 import cherrypy
 from telegram import Bot, Update, ParseMode
@@ -26,31 +25,26 @@ class UniversitasTerbukaBot(object):
 
     def __init__(self, TOKEN: str, NAME: str = None):
         super(UniversitasTerbukaBot, self).__init__()
-        self._updater: Updater = None
         self.TOKEN = TOKEN
         self.NAME = NAME
         self.defaults = Defaults(
             parse_mode=ParseMode.HTML, disable_web_page_preview=True
         )
         # Register handlers
-        self.dp: Dispatcher = None
+        self.updater = Updater(
+            TOKEN, use_context=True, defaults=self.defaults
+        )
+        self.dp = self.updater.dispatcher
+        self.dp.add_error_handler(error_callback)
         self.handlers = Handlers()
+        self.handlers.register(self.dp)
+        self.bot = self.updater.bot
         if NAME:
-            self.bot = Bot(self.TOKEN, defaults=self.defaults)
-            self.update_queue = Queue()
-            self.dp = Dispatcher(self.bot, self.update_queue, use_context=True)
             try:
                 self.bot.setWebhook(
                     "https://{}.herokuapp.com/{}".format(self.NAME, self.TOKEN))
             except:
                 raise RuntimeError("Failed to set the webhook")
-        else:
-            self.updater = Updater(
-                TOKEN, use_context=True, defaults=self.defaults
-            )
-            self.dp = self.updater.dispatcher
-        self.dp.add_error_handler(error_callback)
-        self.handlers.register(self.dp)
 
     @cherrypy.tools.json_in()
     def POST(self, *args, **kwargs):
