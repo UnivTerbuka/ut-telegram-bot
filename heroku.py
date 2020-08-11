222e3e95daca165a575d928551f071902872259d
hexatester <revolusi147id@gmail.com>
2020-08-11 09:38:09 +0700

    use static dir

--- heroku.py
+++ heroku.py
@@ -3,6 +3,14 @@ import os
 import cherrypy
 from core import SimpleWebsite, UniversitasTerbukaBot
 
+BASE_PATH = os.path.dirname(__file__)
+PATH = os.path.abspath(os.path.join(BASE_PATH, 'static'))
+
+
+class Root(object):
+    pass
+
+
 if __name__ == "__main__":
     # Set these variable to the appropriate values
     NAME = os.environ.get('NAME')
@@ -19,7 +27,14 @@ if __name__ == "__main__":
     # Set up the cherrypy configuration
     cherrypy.config.update({'server.socket_host': '0.0.0.0', })
     cherrypy.config.update({'server.socket_port': int(PORT), })
-    cherrypy.tree.mount(SimpleWebsite(), "/")
+    # cherrypy.tree.mount(SimpleWebsite(), "/")
+    cherrypy.tree.mount(Root, '/', config={
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': PATH,
+            'tools.staticdir.index': 'index.html',
+        }
+    })
     cherrypy.tree.mount(UniversitasTerbukaBot(TOKEN, NAME),
                         "/{}".format(TOKEN),
                         {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}})
