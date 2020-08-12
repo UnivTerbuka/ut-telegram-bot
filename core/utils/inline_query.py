184df493c4e0496dd713bbe5295a4d0e0c2524aa
hexatester <revolusi147id@gmail.com>
2020-08-12 17:52:23 +0700

    initial utils

---
+++ core/utils/inline_query.py
@@ -0,0 +1,14 @@
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode, Update
+from uuid import uuid4
+
+def article(title='', description='', message_text='', key=None, reply_markup=None):
+    return InlineQueryResultArticle(
+        id=key or uuid4(),
+        title=title,
+        description=description,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True),
+        reply_markup=reply_markup
+    )
