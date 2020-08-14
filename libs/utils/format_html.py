7891e208837bbbc16a7b20b026147f997be83291
hexatester <revolusi147id@gmail.com>
2020-08-14 13:16:21 +0700

    add italic bold underline

--- libs/utils/format_html.py
+++ libs/utils/format_html.py
@@ -4,3 +4,15 @@ def code(text: str) -> str:
 
 def href(text: str, href: str) -> str:
     return f'<a href="{href}">{text}</a>'
+
+
+def italic(text: str) -> str:
+    return f'<i>{text}</i>'
+
+
+def bold(text: str) -> str:
+    return f'<b>{text}</b>'
+
+
+def underline(text: str) -> str:
+    return f'<u>{text}</u>'
