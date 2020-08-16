b11ce103e3ebfe273b36168e65f885515b45596f
hexatester <revolusi147id@gmail.com>
2020-08-16 11:21:34 +0700

    lazy load module?

--- core/bot.py
+++ core/bot.py
@@ -3,7 +3,6 @@ from flask import Blueprint, request
 from telegram import Bot, Update, ParseMode
 from telegram.ext import Updater,  Dispatcher, Defaults, messagequeue
 from telegram.utils.request import Request
-from handlers import Handlers, error_callback
 
 
 NAME = os.environ.get('NAME')
@@ -53,6 +52,7 @@ class UniversitasTerbukaBot(object):
             TOKEN, use_context=True, defaults=self.defaults
         )
         self.dp: Dispatcher = self.updater.dispatcher
+        from handlers import Handlers, error_callback
         self.dp.add_error_handler(error_callback)
         self.handlers = Handlers()
         self.handlers.register(self.dp)
