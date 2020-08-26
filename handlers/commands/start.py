960124443f8386882149572114320aa8cf5cf8d3
hexatester <revolusi147id@gmail.com>
2020-08-26 22:52:33 +0700

    ignore line too long

--- handlers/commands/start.py
+++ handlers/commands/start.py
@@ -12,8 +12,8 @@ def start(update: Update, context: CallbackContext):
         '/start - Memulai bot\n'
         '/baca - Baca buku\n'
         '/buku - Cari buku\n'
-        '/tiket - Mengecek tiket <a href="http://hallo-ut.ut.ac.id/">hallo-ut</a>\n'
-        '/shortlink - Memendekan url dengan <a href="https://sl.ut.ac.id/">sl-ut</a>\n'
+        '/tiket - Mengecek tiket <a href="http://hallo-ut.ut.ac.id/">hallo-ut</a>\n'  # NOQA
+        '/shortlink - Memendekan url dengan <a href="https://sl.ut.ac.id/">sl-ut</a>\n'  # NOQA
         '/link - Daftar link UT\n'
         '/formulir - Daftar Formulir\n'
         '/about - Tentang bot ini\n')
