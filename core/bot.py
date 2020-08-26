4f9963514ff5bb33c61094e9df14a27f24aa905e
hexatester <revolusi147id@gmail.com>
2020-08-26 22:53:18 +0700

    implement messagequeue

--- core/bot.py
+++ core/bot.py
@@ -1,36 +1,43 @@
 import os
 from flask import Blueprint, request
 from telegram import Bot, Update, ParseMode
-from telegram.ext import Updater,  Dispatcher, Defaults, messagequeue
+from telegram.ext import Updater, Dispatcher, Defaults, messagequeue
 from telegram.utils.request import Request
-
 
 NAME = os.environ.get('NAME')
 TOKEN = os.environ.get('TOKEN')
 
 
-class MQBot(Bot):
+class QueueBot(Bot):
     '''A subclass of Bot which delegates send method handling to MQ'''
-
-    def __init__(self, *args, is_queued_def=True, mqueue=None, **kwargs):
-        super(MQBot, self).__init__(*args, **kwargs)
+    def __init__(self, is_queued_def=True, mqueue=None, **kwargs):
+        super(QueueBot, self).__init__(**kwargs)
         # below 2 attributes should be provided for decorator usage
         self._is_messages_queued_default = is_queued_def
         self._msg_queue = mqueue or messagequeue.MessageQueue(
-            all_burst_limit=3, all_time_limit_ms=3000
-        )
+            all_burst_limit=3, all_time_limit_ms=3000)
 
-    def __del__(self):
+    def stop(self):
         try:
             self._msg_queue.stop()
-        except:
+        except:  # NOQA
             pass
 
     @messagequeue.queuedmessage
     def send_message(self, *args, **kwargs):
         '''Wrapped method would accept new `queued` and `isgroup`
         OPTIONAL arguments'''
-        return super(MQBot, self).send_message(*args, **kwargs)
+        return super(QueueBot, self).send_message(*args, **kwargs)
+
+
+class QueueUpdater(Updater):
+    def __init__(self, bot: QueueBot, *args, **kwargs):
+        super().__init__(bot=bot, *args, **kwargs)
+
+    def signal_handler(self, signum, frame):
+        super().signal_handler(signum, frame)
+
+        self.bot.stop()
 
 
 class UniversitasTerbukaBot(object):
@@ -38,19 +45,23 @@ class UniversitasTerbukaBot(object):
         super(UniversitasTerbukaBot, self).__init__()
         self.TOKEN = TOKEN
         self.NAME = NAME
-        self.defaults = Defaults(
-            parse_mode=ParseMode.HTML, disable_web_page_preview=True
-        )
+        self.defaults = Defaults(parse_mode=ParseMode.HTML,
+                                 disable_web_page_preview=True)
         # Init bot
-        # q = messagequeue.MessageQueue(
-        #     all_burst_limit=3, all_time_limit_ms=3000
-        # )
-        # request = Request(con_pool_size=8)
-        # self.bot = MQBot(TOKEN, defaults=self.defaults, request=request, mqueue=q)
+        q = messagequeue.MessageQueue(all_burst_limit=3,
+                                      all_time_limit_ms=3000)
+        request = Request(con_pool_size=8)
+        self.bot = QueueBot(token=TOKEN,
+                            request=request,
+                            mqueue=q,
+                            defaults=self.defaults)
         # Register handlers
-        self.updater: Updater = Updater(
-            TOKEN, use_context=True, defaults=self.defaults
-        )
+        self.updater: QueueUpdater = QueueUpdater(self.bot,
+                                                  use_context=True,
+                                                  defaults=self.defaults)
+        # self.updater: Updater = Updater(
+        #     TOKEN, use_context=True, defaults=self.defaults
+        # )
         self.dp: Dispatcher = self.updater.dispatcher
         from handlers import Handlers, error_callback
         self.dp.add_error_handler(error_callback)
@@ -58,11 +69,8 @@ class UniversitasTerbukaBot(object):
         self.handlers.register(self.dp)
         self.bot = self.updater.bot
         if NAME:
-            try:
-                self.bot.setWebhook(
-                    "https://{}.herokuapp.com/{}".format(self.NAME, self.TOKEN))
-            except:
-                raise RuntimeError("Failed to set the webhook")
+            self.bot.setWebhook("https://{}.herokuapp.com/{}".format(
+                self.NAME, self.TOKEN))
 
     def process_update(self, update):
         update = Update.de_json(update, self.bot)
@@ -81,4 +89,5 @@ def get_blueprint(token, name):
         update = request.get_json()
         bot.process_update(update)
         return ''
+
     return bp
