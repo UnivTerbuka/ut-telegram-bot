4d11964be27a981f9f3fb04c781c637a6242b91e
hexatester <revolusi147id@gmail.com>
2020-08-26 22:58:56 +0700

    change about url

--- handlers/commands/about.py
+++ handlers/commands/about.py
@@ -4,7 +4,7 @@ from core.utils import action
 
 MESSAGE = '''
 Developer : @hexatester - Habib Rohman
-Source code : https://github.com/hexatester/ut-telegram-bot
+Source code : https://github.com/UnivTerbuka/ut-telegram-bot
 
 Bot ini dikembangan dengan menggunakan <a href="https://www.python.org/">Bahasa Pemrograman Python </a>.
 
@@ -12,7 +12,7 @@ Dan menggunakan library :
 * <a href="https://python-telegram-bot.org/">python-telegram-bot</a>
 * <a href="https://www.crummy.com/software/BeautifulSoup/">beautifulsoup4</a>
 * <a href="https://2.python-requests.org/en/master/">requests</a>
-* <a href="https://github.com/hexatester/ut-telegram-bot/blob/master/requirements.txt">dll.</a>
+* <a href="https://github.com/UnivTerbuka/ut-telegram-bot/blob/master/requirements.txt">dll.</a>
 '''
 
 
