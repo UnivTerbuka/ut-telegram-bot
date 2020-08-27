a1d29d37cdccbf7038a4e1f9c46453b46f4e853d
hexatester <revolusi147id@gmail.com>
2020-08-27 17:46:32 +0700

    disable action

--- core/utils/action.py
+++ core/utils/action.py
@@ -8,8 +8,8 @@ def send_action(action):
     def decorator(func):
         @wraps(func)
         def command_func(update, context, *args, **kwargs):
-            context.bot.send_chat_action(
-                chat_id=update.effective_message.chat_id, action=action)
+            # context.bot.send_chat_action(
+            #     chat_id=update.effective_message.chat_id, action=action)
             return func(update, context,  *args, **kwargs)
         return command_func
     return decorator
