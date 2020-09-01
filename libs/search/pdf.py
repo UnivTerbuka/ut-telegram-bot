e69f65f1bd01e9773bd9064ba9d9b51b105527d1
hexatester <revolusi147id@gmail.com>
2020-09-01 23:08:48 +0700

    add searching pdf

---
+++ libs/search/pdf.py
@@ -0,0 +1,47 @@
+import os
+import yaml
+from dacite import from_dict
+from dataclasses import dataclass
+from telegram import (InlineQueryResultDocument, InputTextMessageContent,
+                      InlineKeyboardButton, InlineKeyboardMarkup)
+from typing import List
+from uuid import uuid4
+from config import ROOT_PATH
+from libs.utils.format_html import href
+
+FILENAME = 'pdf.yaml'
+
+
+@dataclass
+class Pdf:
+    title: str
+    topic: str
+    url: str
+    caption: str = ''
+
+    def __post_init__(self):
+        assert self.url.endswith('.pdf')
+        self.caption = self.caption or self.title
+        self.text = href(self.caption, self.url)
+        self.reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text='Download', url=self.url)]])
+
+    @property
+    def result_document(self) -> InlineQueryResultDocument:
+        return InlineQueryResultDocument(
+            id=uuid4(),
+            title=self.title,
+            description=self.topic,
+            document_url=self.url,
+            mime_type='application/pdf',
+            input_message_content=InputTextMessageContent(
+                self.text, disable_web_page_preview=False),
+            reply_markup=self.reply_markup)
+
+
+def get_pdf() -> List[Pdf]:
+    pdf_yaml = os.path.join(ROOT_PATH, FILENAME)
+
+    with open(pdf_yaml, 'rb') as f:
+        datas = yaml.load(f, Loader=yaml.FullLoader)
+    return [from_dict(Pdf, data) for data in datas if data]
--- libs/search/search.py
+++ libs/search/search.py
@@ -1,35 +1,57 @@
 import logging
 from fuzzywuzzy import process
-from typing import List
+from typing import List, Type
 from telegram import InlineQueryResult
 from .faq import QnA, get_qna
+from .pdf import Pdf, get_pdf
 
 
 class Search:
     def __init__(self):
         self.qna: List[QnA] = get_qna()
+        self.pdf: List[Pdf] = get_pdf()
         self.qna_q_dict = dict(enumerate([qna.question for qna in self.qna]))
         self.qna_a_dict = dict(enumerate([qna.answer for qna in self.qna]))
+        self.pdf_t_dict = dict(enumerate([pdf.title for pdf in self.pdf]))
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    def __call__(self, query: str, thres=3) -> List[InlineQueryResult]:
+    def __call__(self, query: str, thres=3) -> List[Type[InlineQueryResult]]:
         try:
             if not query:
                 return []
             self.logger.debug('Searching {}'.format(query))
-            best_q = process.extractBests(query,
-                                          self.qna_q_dict,
-                                          score_cutoff=50,
-                                          limit=20)
-            best_a = process.extractBests(query,
-                                          self.qna_a_dict,
-                                          score_cutoff=50)
-            results: List[InlineQueryResult] = []
-            results += [self.qna[z].result_article
-                        for (x, y, z) in best_q] if best_q else []
-            results += [self.qna[z].result_article
-                        for (x, y, z) in best_a] if best_a else []
+            results: List[Type[InlineQueryResult]] = []
+            results.extend(self.best_qna(query))
+            results.extend(self.best_pdf(query))
             return results
         except Exception as E:
             self.logger.exception(E)
             return []
+
+    def best_qna(self,
+                 query: str,
+                 score_cutoff: int = 55) -> List[Type[InlineQueryResult]]:
+        best_q = process.extractBests(query,
+                                      self.qna_q_dict,
+                                      score_cutoff=score_cutoff,
+                                      limit=10)
+        best_a = process.extractBests(query,
+                                      self.qna_a_dict,
+                                      score_cutoff=score_cutoff + 10,
+                                      limit=5)
+        results: List[Type[InlineQueryResult]] = []
+        results += [self.qna[z].result_article
+                    for (x, y, z) in best_q] if best_q else []
+        results += [self.qna[z].result_article
+                    for (x, y, z) in best_a] if best_a else []
+        return results
+
+    def best_pdf(self,
+                 query: str,
+                 score_cutoff: int = 50) -> List[Type[InlineQueryResult]]:
+        best_f = process.extractBests(query,
+                                      self.pdf_t_dict,
+                                      score_cutoff=score_cutoff,
+                                      limit=5)
+        return [self.pdf[z].result_document
+                for (x, y, z) in best_f] if best_f else []
