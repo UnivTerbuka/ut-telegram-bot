e235b9b2e77918bedccfd9f7fc9e17e9f6b2b0a3
hexatester <revolusi147id@gmail.com>
2020-09-02 21:54:29 +0700

    Add Rss as default inline results

--- handlers/inlines/search.py
+++ handlers/inlines/search.py
@@ -2,9 +2,15 @@ from telegram import Update, InlineQuery, InlineQueryResult
 from telegram.ext import CallbackContext
 from typing import List
 from core.utils.inline_query import article
+from libs.rss.rss import Rss
 from libs.search.search import Search
 
+RSS = Rss()
 SEARCH = Search()
+EMPTY = article(
+    title='❌ Tidak ada hasil',
+    description='',
+    message_text='Pm @UniversitasTerbukaBot untuk mengakses layanan UT. 😁')
 
 
 def search(update: Update, context: CallbackContext):
@@ -13,13 +19,12 @@ def search(update: Update, context: CallbackContext):
     results_list: List[InlineQueryResult] = []
     if len(query) > 0:
         results_list.extend(SEARCH(query))
+        results_list.extend(RSS(query))
     if not results_list:
-        results_list.append(
-            article(
-                title='❌ Tidak ada hasil',
-                description='',
-                message_text='Silahkan kunjungi @UniversitasTerbukaBot untuk mengakses layanan UT dari Telegram. 😁'  # NOQA
-            ))
+        if RSS.inline_results:
+            results_list.extend(RSS.inline_results)
+        else:
+            results_list.append(EMPTY)
         inline_query.answer(results_list,
                             switch_pm_text='Bantuan',
                             switch_pm_parameter='inline-help')
