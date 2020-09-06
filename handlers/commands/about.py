576c32e6f66b1673a836d69a98de31e7af797826
hexatester <revolusi147id@gmail.com>
2020-09-06 18:52:19 +0700

    update about

--- handlers/commands/about.py
+++ handlers/commands/about.py
@@ -1,19 +1,26 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 from core.utils import action
+from libs.utils import format_html
 
 MESSAGE = '''
-Developer : @hexatester - Habib Rohman
+Developers :
+Habib Rohman - @hexatester - {linkedin}
+
 Source code : https://github.com/UnivTerbuka/ut-telegram-bot
 
-Bot ini dikembangan dengan menggunakan <a href="https://www.python.org/">Bahasa Pemrograman Python </a>.
+Bot ini dikembangan dengan menggunakan {python}.
 
 Dan menggunakan library :
 * <a href="https://python-telegram-bot.org/">python-telegram-bot</a>
 * <a href="https://www.crummy.com/software/BeautifulSoup/">beautifulsoup4</a>
 * <a href="https://2.python-requests.org/en/master/">requests</a>
-* <a href="https://github.com/UnivTerbuka/ut-telegram-bot/blob/master/requirements.txt">dll.</a>
-'''
+* {dll}
+'''.format(linkedin=format_html.href(
+    "LinkedIn", "https://sl.ut.ac.id/hexatester-linkedin"),
+           python=format_html.href("Bahasa Pemrograman Python",
+                                   "https://sl.ut.ac.id/python"),
+           dll=format_html.href("dll", "https://sl.ut.ac.id/ut-bot-req"))
 
 
 @action.typing
