0b94853933eaffb5ff954260cb97d98d30d4fc26
hexatester <revolusi147id@gmail.com>
2020-09-08 20:50:30 +0700

    v4.7.0

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
 from .bot import UniversitasTerbukaBot
+from .version import __version__  # NOQA
 
 __all__ = ['UniversitasTerbukaBot']
---
+++ tests/core_test.py
@@ -0,0 +1,5 @@
+from core import __version__
+
+
+def test_version():
+    assert __version__ == '4.7.0'
