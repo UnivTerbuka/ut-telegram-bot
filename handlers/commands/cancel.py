c3b96380c2af285a8682ae61b549e1f32229e660
hexatester <revolusi147id@gmail.com>
2020-09-16 23:13:23 +0700

    add ReplyKeyboardRemove

--- handlers/commands/cancel.py
+++ handlers/commands/cancel.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import CallbackContext, Job
 from core.utils import action
 
@@ -12,5 +12,8 @@ def cancel(update: Update, context: CallbackContext):
     if 'job' in context.chat_data:
         job: Job = context.chat_data['job']
         job.schedule_removal()
-    update.effective_message.reply_text(MESSAGE)
+    update.effective_message.reply_text(
+        MESSAGE,
+        reply_markup=ReplyKeyboardRemove(),
+    )
     return -1
