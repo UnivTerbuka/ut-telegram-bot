db03e913a580fda2b076f932a36e771ffe5ba039
hexatester <revolusi147id@gmail.com>
2020-09-25 23:37:05 +0700

    refactor course

--- handlers/callbacks/course.py
+++ handlers/callbacks/course.py
@@ -1,10 +1,13 @@
 from logging import getLogger
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+
+from moodle.core.course import BaseCourse
+
 from core.context import CoreContext
 from core.decorator import assert_token
 from core.session import message_wrapper
 from libs.elearning.course import course_text
-from libs.utils.helpers import make_button
+from libs.utils.helpers import build_menu, make_data
 from config import CALLBACK_SEPARATOR
 
 logger = getLogger(__name__)
@@ -17,15 +20,23 @@ def course(update: Update, context: CoreContext):
     datas = context.query.data.split(CALLBACK_SEPARATOR)
     # COURSE|course_id
     course_id = int(datas[-1])
-    try:
-        course = context.moodle.core.course.get_courses_by_field(
-            'id', course_id)[0]
-    except Exception as e:
-        logger.debug('Error {}'.format(repr(e)))
-        context.query.edit_message_text('Gagal mendapatkan kursus')
+    courses = BaseCourse(context.moodle).get_courses_by_field('id', course_id)
+    if not courses:
+        context.query.edit_message_text('Kursus tidak ditemukan.')
         return -1
+    course_ = courses[0]
+    buttons = [
+        InlineKeyboardButton('Daftar Sesi',
+                             callback_data=make_data('CONTENTS', course_id)),
+        InlineKeyboardButton('Daftar Forum',
+                             callback_data=make_data('FORUMS', course_id))
+    ]
+    keyboard = build_menu(
+        buttons,
+        footer_buttons=InlineKeyboardButton('Tutup', callback_data='CLOSE'),
+    )
     context.query.edit_message_text(
-        text=course_text(course),
-        reply_markup=make_button('Daftar Forum', f"FORUMS|{course_id}"),
+        text=course_text(course_),
+        reply_markup=InlineKeyboardMarkup(keyboard),
     )
     return -1
