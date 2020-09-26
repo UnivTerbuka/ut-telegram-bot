5e7c3b2899ee5a8df6d76d760ac8d7717ee2fbb1
hexatester <revolusi147id@gmail.com>
2020-09-26 12:24:52 +0700

    improve pluginfile

--- handlers/inlines/pluginfile.py
+++ handlers/inlines/pluginfile.py
@@ -1,7 +1,9 @@
-from telegram import Update, InlineQuery, InlineQueryResultDocument
+from telegram import (Update, InlineQuery, InlineQueryResultArticle,
+                      InlineQueryResultDocument, InputTextMessageContent)
 from core.context import CoreContext
 from core.decorator import assert_token
 from core.session import message_wrapper
+from libs.utils import format_html
 
 
 @message_wrapper
@@ -9,16 +11,39 @@ from core.session import message_wrapper
 def pluginfile(update: Update, context: CoreContext):
     query: InlineQuery = update.inline_query
     url: str = query.query
-    # https://elearning.ut.ac.id/webservice/pluginfile.php/9661772/mod_resource/content/0/RAT.pdf
+    # https://elearning.ut.ac.id/webservice/pluginfile.php/12345/mod_resource/content/0/File.pdf
     urls = url.split('/')
-    result = InlineQueryResultDocument(
-        id=urls[5],
-        document_url=url + '?token=' + context.moodle.token,
-        title=urls[-1],
-        mime_type='application/pdf',
-        description='File elearning.',
-    )
-    results = [result]
+    resource_id = urls[5]
+    resource_title = urls[-1].title()
+    resource_url = url + '?token=' + context.moodle.token
+    results = list()
+    if url.endswith('.pdf'):
+        result = InlineQueryResultDocument(
+            id=resource_id,
+            document_url=resource_url,
+            title=resource_title,
+            mime_type='application/pdf',
+            description='File elearning.',
+        )
+        results.append(result)
+    elif url.endswith('.zip'):
+        result = InlineQueryResultDocument(
+            id=resource_id,
+            document_url=resource_url,
+            title=resource_title,
+            mime_type='application/zip',
+            description='File elearning.',
+        )
+        results.append(result)
+    else:
+        result = InlineQueryResultArticle(
+            id=resource_id,
+            title=resource_title,
+            input_message_content=InputTextMessageContent(
+                resource_title + format_html.href('\u200c', resource_url),
+                disable_web_page_preview=False,
+            ))
+        results.append(result)
     query.answer(
         results,
         cache_time=600,
@@ -29,4 +54,4 @@ def pluginfile(update: Update, context: CoreContext):
     return -1
 
 
-pluginfile_pattern = r'^https:\/\/elearning\.ut\.ac\.id\/webservice\/pluginfile\.php\/\d+\/mod_resource\/content\/\d\/\S+\.pdf$'  # NOQA
+pluginfile_pattern = r'^https:\/\/elearning\.ut\.ac\.id\/webservice\/pluginfile\.php\/\d+\/mod_resource\/content\/\d\/\S+$'  # NOQA
