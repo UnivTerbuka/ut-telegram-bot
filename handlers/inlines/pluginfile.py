8025af7e1c9cc301beb71a36b5a40d52d2ce01ef
hexatester <revolusi147id@gmail.com>
2020-09-26 12:41:15 +0700

    improve resource_title

--- handlers/inlines/pluginfile.py
+++ handlers/inlines/pluginfile.py
@@ -1,5 +1,7 @@
 from telegram import (Update, InlineQuery, InlineQueryResultArticle,
                       InlineQueryResultDocument, InputTextMessageContent)
+from urllib.parse import unquote
+
 from core.context import CoreContext
 from core.decorator import assert_token
 from core.session import message_wrapper
@@ -14,7 +16,7 @@ def pluginfile(update: Update, context: CoreContext):
     # https://elearning.ut.ac.id/webservice/pluginfile.php/12345/mod_resource/content/0/File.pdf
     urls = url.split('/')
     resource_id = urls[5]
-    resource_title = urls[-1].title()
+    resource_title = unquote(urls[-1]).title()
     resource_url = url + '?token=' + context.moodle.token
     results = list()
     if url.endswith('.pdf'):
@@ -40,9 +42,11 @@ def pluginfile(update: Update, context: CoreContext):
             id=resource_id,
             title=resource_title,
             input_message_content=InputTextMessageContent(
-                resource_title + format_html.href('\u200c', resource_url),
+                format_html.href(resource_title, resource_url),
                 disable_web_page_preview=False,
-            ))
+            ),
+            description='File elearning.',
+        )
         results.append(result)
     query.answer(
         results,
