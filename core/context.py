9c3905b129c8623897ea1f0a44ad66d68b2e04f7
hexatester <revolusi147id@gmail.com>
2020-09-29 13:40:24 +0700

    initial CoreChat CoreMessage CoreQuery

--- core/context.py
+++ core/context.py
@@ -1,10 +1,45 @@
 from __future__ import annotations
 from moodle import Mdl
 from sqlalchemy.orm import Session
-from telegram import Chat, Message, Update, CallbackQuery
+from telegram import (Chat, Message, Update, CallbackQuery, ParseMode)
 from telegram.ext import CallbackContext, Dispatcher
 from core.models import User
 from config import MOODLE_URL
+
+
+class CoreDefault(object):
+    def defaults(self, kwargs: dict) -> None:
+        if 'parse_mode' not in kwargs:
+            kwargs['parse_mode'] = ParseMode.HTML
+        if 'disable_web_page_preview' not in kwargs:
+            kwargs['disable_web_page_preview'] = True
+
+
+class CoreChat(Chat, CoreDefault):
+    def __init__(self, chat: Chat):
+        super(CoreChat, self).__init__(**chat.to_dict())
+
+    def send_message(self, *args, **kwargs):
+        self.defaults(kwargs)
+        return super().send_message(*args, **kwargs)
+
+
+class CoreMessage(Message, CoreDefault):
+    def __init__(self, message: Message):
+        super(CoreMessage, self).__init__(**message.to_dict())
+
+    def reply_text(self, *args, **kwargs):
+        self.defaults(kwargs)
+        return super().reply_text(*args, **kwargs)
+
+
+class CoreQuery(CallbackQuery, CoreDefault):
+    def __init__(self, query: CallbackQuery):
+        super(CoreQuery, self).__init__(**query.to_dict())
+
+    def edit_message_text(self, *args, **kwargs):
+        self.defaults(kwargs)
+        return super().edit_message_text(*args, **kwargs)
 
 
 class CoreContext(CallbackContext):
@@ -54,9 +89,9 @@ class CoreContext(CallbackContext):
                   session: Session, user: User) -> CoreContext:
         self = cls(context.dispatcher)
 
-        self._chat = update.effective_chat
-        self._message = update.effective_message
-        self._query = update.callback_query
+        self._chat = CoreChat(update.effective_chat)
+        self._message = CoreMessage(update.effective_message)
+        self._query = CoreQuery(update.callback_query)
 
         self._session = session
         self._user = user
