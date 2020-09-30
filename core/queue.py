e8459210890e436ad194ec34af02a6dedd990424
hexatester <revolusi147id@gmail.com>
2020-09-30 21:37:15 +0700

    black

--- core/queue.py
+++ core/queue.py
@@ -9,7 +9,7 @@ from telegram.utils.helpers import DEFAULT_NONE
 class CoreQueueBot(Bot):
     def __new__(cls, *args, **kwargs):
         # Get default values from kwargs
-        defaults = kwargs.get('defaults')
+        defaults = kwargs.get("defaults")
 
         # Make an instance of the class
         instance = super().__new__(cls)
@@ -18,23 +18,29 @@ class CoreQueueBot(Bot):
             return instance
 
         # For each method ...
-        for method_name, method in inspect.getmembers(instance, predicate=inspect.ismethod):
+        for method_name, method in inspect.getmembers(
+            instance, predicate=inspect.ismethod
+        ):
             # ... get kwargs
             argspec = inspect.getfullargspec(inspect.unwrap(method))
-            kwarg_names = argspec.args[-len(argspec.defaults or []):]
+            kwarg_names = argspec.args[-len(argspec.defaults or []) :]  # NOQA
             # ... check if Defaults has a attribute that matches the kwarg name
             needs_default = [
-                kwarg_name for kwarg_name in kwarg_names if hasattr(defaults, kwarg_name)
+                kwarg_name
+                for kwarg_name in kwarg_names
+                if hasattr(defaults, kwarg_name)
             ]
             # ... make a dict of kwarg name and the default value
             default_kwargs = {
-                kwarg_name: getattr(defaults, kwarg_name) for kwarg_name in needs_default if (
-                    getattr(defaults, kwarg_name) is not DEFAULT_NONE
-                )
+                kwarg_name: getattr(defaults, kwarg_name)
+                for kwarg_name in needs_default
+                if (getattr(defaults, kwarg_name) is not DEFAULT_NONE)
             }
             # ... apply the defaults using a partial
             if default_kwargs:
-                setattr(instance, method_name, functools.partial(method, **default_kwargs))
+                setattr(
+                    instance, method_name, functools.partial(method, **default_kwargs)
+                )
 
         return instance
 
@@ -50,17 +56,19 @@ class CoreQueueBot(Bot):
             pass
 
     @queuedmessage
-    def send_message(self,
-                     chat_id,
-                     text,
-                     parse_mode=None,
-                     disable_web_page_preview=None,
-                     disable_notification=False,
-                     reply_to_message_id=None,
-                     reply_markup=None,
-                     timeout=None,
-                     **kwargs):
-        '''Wrapped method would accept new `queued` and `isgroup` OPTIONAL arguments'''
+    def send_message(
+        self,
+        chat_id,
+        text,
+        parse_mode=None,
+        disable_web_page_preview=None,
+        disable_notification=False,
+        reply_to_message_id=None,
+        reply_markup=None,
+        timeout=None,
+        **kwargs
+    ):
+        """Wrapped method would accept new `queued` and `isgroup` OPTIONAL arguments"""
         # self.add_defaults(kwargs)
         return super(CoreQueueBot, self).send_message(
             chat_id,
