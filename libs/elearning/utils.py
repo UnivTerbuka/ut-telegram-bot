3bb5dfdf405d4b4d9647537fdace5adc78aed21b
hexatester <revolusi147id@gmail.com>
2020-10-04 23:06:08 +0700

    improve is_valid_token

--- libs/elearning/utils.py
+++ libs/elearning/utils.py
@@ -4,6 +4,8 @@ import requests
 def is_valid_token(
     token: str, url="https://elearning.ut.ac.id/webservice/rest/server.php"
 ) -> bool:
+    if len(token) != 32:
+        return False
     params = {"wstoken": token, "wsfunction": "core_webservice_get_site_info"}
     res = requests.get(url, params=params)
     return "invalidtoken" not in res.text if res.ok else False
