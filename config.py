533eab84333e3bbf70c7d2353e8b6e79a54ba524
hexatester <revolusi147id@gmail.com>
2020-10-14 01:25:54 +0700

    fix presistence

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ROOT_PATH = get_script_path()
 STATIC_PATH = os.path.join(ROOT_PATH, "static")
 IMG_PATH = os.path.join(STATIC_PATH, "images")
 RES_PATH = os.path.join(STATIC_PATH, "resources")
-PERSISTENCE = PicklePersistence("data.persist")
+PERSISTENCE = PicklePersistence("data.persist", store_user_data=False)
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0"  # NOQA
 }
