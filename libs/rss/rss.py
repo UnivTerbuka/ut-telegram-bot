8040c1d1a4023e4d7b7184423d51a003f57ac57a
hexatester <revolusi147id@gmail.com>
2020-10-17 23:13:23 +0700

    refactor Rss

--- libs/rss/rss.py
+++ libs/rss/rss.py
@@ -8,13 +8,11 @@ class Rss:
     def __init__(self):
         self.pengumuman: List[Pengumuman] = get_pengumuman()
         self.pengumuman_title_dict = dict(enumerate([p.title for p in self.pengumuman]))
-        inline_results: List[Type[InlineQueryResult]] = []
-        inline_results += (
+        self.inline_results: List[Type[InlineQueryResult]] = (
             [pengumuman.result_article for pengumuman in self.pengumuman]
             if self.pengumuman
             else []
         )
-        self.inline_results = inline_results
 
     def __call__(self, query: str) -> List[Type[InlineQueryResult]]:
         return self.search(query)
@@ -23,10 +21,9 @@ class Rss:
         best_p_t = process.extractBests(
             query, self.pengumuman_title_dict, score_cutoff=40, limit=3
         )
-        results: List[Type[InlineQueryResult]] = []
-        results += (
+        results: List[Type[InlineQueryResult]] = (
             [self.pengumuman[z].result_article for (x, y, z) in best_p_t]
             if best_p_t
             else []
         )
-        return results
+        return results or []
