5f1886ba916315a5cb5bb4e0583bfed232b1c584
hexatester <revolusi147id@gmail.com>
2020-10-17 23:28:20 +0700

    add callback_elearning

--- core/decorator.py
+++ core/decorator.py
@@ -1,9 +1,11 @@
 from functools import wraps
+from requests.exceptions import ConnectionError
 from telegram import Update, User
 from telegram.ext import CallbackContext
 from typing import Any, Callable, List
 
 from . import CoreContext
+from libs.utils.helpers import make_button
 from config import DOMAIN
 
 
@@ -40,5 +42,24 @@ def assert_token(
                 context.message.reply_text(msg)
             return -1
         return func(update, context)
+
+    return wrapper
+
+
+def callback_elearning(
+    func: Callable[[Update, CoreContext], Any]
+) -> Callable[[Update, CoreContext], Any]:
+    @wraps(func)
+    def wrapper(update: Update, context: CoreContext) -> Any:
+        result = None
+        try:
+            result = func(update, context)
+        except ConnectionError:
+            callback_query = update.callback_query
+            callback_query.edit_message_text(
+                "Tidak dapat menghubungi elearning, coba beberapa saat lagi.",
+                reply_markup=make_button('Coba lagi', callback_query.data)
+            )
+        return result
 
     return wrapper
