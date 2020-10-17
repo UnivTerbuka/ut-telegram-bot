6d066317fcadb995091a537ccc4f7053db9dd5b5
hexatester <revolusi147id@gmail.com>
2020-10-17 22:45:59 +0700

    refactor type hint

--- handlers/inlines/bahan_ajar.py
+++ handlers/inlines/bahan_ajar.py
@@ -1,12 +1,11 @@
 from telegram import Update, InlineQuery
 from telegram.ext import CallbackContext
-from typing import List
 from libs.bahan_ajar import BahanAjar, Book
 
 
 def bahan_ajar(update: Update, context: CallbackContext):
     query: InlineQuery = update.inline_query
-    bahan_ajars: List[Book] = BahanAjar.search(query.query)
+    bahan_ajars = BahanAjar.search(query.query)
     results = []
     if bahan_ajars:
         buku: Book = bahan_ajars[0]
